a9125d001d91beaeba2d49b6199e963a7d35ec83
Xue Li <xli26@nd.edu>
2018-04-05 14:25:50 -0400

    Add interface of mesh partation method

--- parmetismodule.c
+++ parmetismodule.c
@@ -136,12 +136,116 @@ parmetis_PyParMETIS_V3_PartKway(PyObject *self, PyObject *args, PyObject *keywds
 	return Py_BuildValue("iiO", err, edgecut, pypart);
 }
 
+static PyObject *
+parmetis_PyParMETIS_V3_PartMeshKway(PyObject *self, PyObject *args, PyObject *keywds)
+{
+	PyArrayObject *pyelmdist = NULL;
+	PyArrayObject *pyeptr = NULL;
+	PyArrayObject *pyeind = NULL;
+	PyArrayObject *pyelmwgt = NULL;
+	PyArrayObject *pytpwgts = NULL;
+	PyArrayObject *pyubvec = NULL;
+	PyArrayObject *pyoptions = NULL;
+	idx_t wgtflag = 0;
+	idx_t numflag = 0;
+	idx_t ncon = 1;
+	idx_t ncommonnodes = 2; // two for triangular meshes (Default)
+	idx_t nparts = 0; // necessary
+
+	idx_t *elmdist = NULL;
+	idx_t *eptr = NULL;
+	idx_t *eind = NULL;
+	idx_t *elmwgt = NULL;
+	real_t *tpwgts = NULL;
+	real_t *ubvec = NULL;
+	idx_t dfoptions[] = {0, 0, 0};
+	idx_t *options = dfoptions;
+
+	PyObject *pycomm = NULL;
+	MPI_Comm *pcomm = NULL;
+
+	static char *kwlist[] = {
+		"elmdist", "eptr", "eind", "nparts", "comm",
+		"elmwgt", "wgtflag", "numflag", "ncon", "ncommonnodes",
+		"tpwgts", "ubvec", "options", NULL};
+	if (!PyArg_ParseTupleAndKeywords(
+		args, keywds, "O!O!O!iO|O!iiiiO!O!O!", kwlist,
+		&PyArray_Type, &pyelmdist, &PyArray_Type, &pyeptr,
+		&PyArray_Type, &pyeind, &nparts, &pycomm,
+		&PyArray_Type, &pyelmwgt, &wgtflag, &numflag,
+		&ncon, &ncommonnodes, &PyArray_Type, &pytpwgts,
+		&PyArray_Type, &pyubvec, &PyArray_Type, &pyoptions))
+		return NULL;
+
+	pcomm = PyMPIComm_Get(pycomm);
+	if (NULL == pcomm)
+		return NULL;
+
+	elmdist = cintarray(pyelmdist);
+	eptr = cintarray(pyeptr);
+	eind = cintarray(pyeind);
+	if (NULL != pyelmdist)
+		elmwgt = cintarray(pyelmwgt);
+	if (NULL != pytpwgts)
+	{
+		tpwgts = cdoublearray(pytpwgts);
+	} else {
+		tpwgts = (double *)malloc(sizeof(double) * ncon * nparts);
+		for (int itp = 0; itp < ncon * nparts; ++itp)
+		{
+			tpwgts[itp] = 1.0 / (double)nparts;
+		}
+	}
+	if (NULL != pyubvec)
+	{
+		ubvec = cdoublearray(pyubvec);
+	} else {
+		ubvec = (double *)malloc(sizeof(double) * ncon);
+		for (int iub = 0; iub < ncon; ++iub)
+		{
+			ubvec[iub] = 1.05;
+		}
+	}
+	if (NULL != pyoptions)
+	{
+		options = cintarray(pyoptions);
+	}
+
+	// Prepare the return variables.
+	int nelm = pyeptr->dimensions[0] - 1;
+    idx_t *part = (int64_t *)malloc(sizeof(int64_t) * nelm);
+    idx_t edgecut;
+
+	int err = ParMETIS_V3_PartMeshKway(
+		elmdist, eptr, eind, elmwgt, &wgtflag,
+		&numflag, &ncon, &ncommonnodes, &nparts,
+		tpwgts, ubvec, options, &edgecut, part, pcomm);
+
+	PyArrayObject *pypart =
+		(PyArrayObject *)PyArray_FromDims(1, &nelm, NPY_INT64);
+	uint64_t *pypartdata = (uint64_t *)pypart->data;
+	for (int ipart = 0; ipart < nelm; ++ipart)
+	{
+		pypartdata[ipart] = part[ipart];
+	}
+
+	free(part);
+	if (NULL == pytpwgts) free(tpwgts);
+	if (NULL == pyubvec) free(ubvec);
+
+	return Py_BuildValue("iiO", err, edgecut, pypart);
+}
+
 // The module's methods table and initialize function
 static PyMethodDef ParmetisMethods[] = {
 	{"PyParMETIS_V3_PartKway",
 		(PyCFunction) parmetis_PyParMETIS_V3_PartKway,
 		METH_VARARGS | METH_KEYWORDS,
 		"Partation the graph parallelly."},
+	{"PyParMETIS_V3_PartMeshKway",
+		(PyCFunction) parmetis_PyParMETIS_V3_PartMeshKway,
+		METH_VARARGS | METH_KEYWORDS,
+		"Partation the mesh parallelly."},
 	{NULL, NULL, 0, NULL}
 };
 
